db09931184aaa97de353e364100effbb7f5e4fa4
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-08 19:08:18 -0800

    Completed.

--- War Challenge/ContentView.swift
+++ War Challenge/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
 	var body: some View {
 		VStack {
 			Spacer()
@@ -15,20 +21,37 @@ struct ContentView: View {
 			Spacer()
 			HStack {
 				Spacer()
-				Image("card3")
+				Image(playerCard)
 				Spacer()
-				Image("card4")
+				Image(cpuCard)
 				Spacer()
 			}//HStack
 			Spacer()
-			Image("dealbutton")
+            Button(action: {
+                let playerRand = Int.random(in: 2...14)
+                let cpuRand = Int.random(in: 2...14)
+                
+                //update the cards
+                playerCard = "card\(playerRand)"
+                cpuCard = "card\(cpuRand)"
+                
+                //update the score
+                if playerRand > cpuRand {
+                    playerScore += 1
+                }
+                else if cpuRand > playerRand {
+                    cpuScore += 1
+                }
+            }, label: {
+                Image("dealbutton")
+            })
 			Spacer()
 			HStack {
 				Spacer()
 				VStack {
 					Text("Player")
 						.font(.headline)
-					Text("0")
+					Text(String(playerScore))
 						.padding()
 						.font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
 				}//VStack
@@ -36,7 +59,7 @@ struct ContentView: View {
 				VStack {
 					Text("CPU")
 						.font(.headline)
-					Text("0")
+					Text(String(cpuScore))
 						.padding()
 						.font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
 				}//VStack
